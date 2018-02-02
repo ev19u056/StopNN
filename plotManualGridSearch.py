0d6c23945575fc77c1cd491b3d67991a70abccf7
diogodebastos <diogodebastos18@gmail.com>
2018-02-02 17:42:44 +0000

    Parse arguments for plotting manual grid search

--- plotManualGridSearch.py
+++ plotManualGridSearch.py
@@ -1,77 +1,79 @@
-#from matplotlib.colors import LogNorm
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import localConfig as cfg
-#from prepareDATA import test_point
 
-runNum = 11
-learning_rate = 5
-my_decay = 0
-test_point = "550_520"
-filepath = cfg.lgbk+"Searches/run"+str(runNum)
-os.chdir(filepath)
+if __name__ == "__main__":
+    import argparse
+    import sys
 
-#f = open(filepath+name+'.txt', 'r')
-# Until run5
-#name = "mGS:outputs_run"+str(runNum)+"_"+test_point
-name = "mGS:outputs_run"+str(runNum)+"_"+test_point+"_"+str(learning_rate)+"_"+str(my_decay)
-
-f = open(name + '.txt', 'r')
-
-layer = []
-neurons = []
-roc_AUC = []
-ks = []
-ks_s = []
-ks_b = []
-FOM = []
-line_index=0
-
-# ===> AQUI CRIA-SE LISTAS DOS VALORES. CONVEM AJUSTAR PAAR QUAIS SE GUARDARAM ETC. NESTE CASO ESTAO 7 A SER GUARDADOS.
-for line in f:
-    if line_index%7==0:
-        layer.append(float(line,))
-    if line_index%7==1:
-        neurons.append(float(line,))
-    if line_index%7==2:
-        roc_AUC.append(float(line,))
-    if line_index%7==3:
-        ks_s.append(float(line,))
-    if line_index%7==4:
-        ks_b.append(float(line,))
-    if line_index%7==5:
-        ks.append(float(line,))
-    if line_index%7==6:
-        FOM.append(float(line,))
-    line_index=line_index+1
-
-layers_legend = ["1 layer","2 layers", "3 layers"]
-nLayers = len(layers_legend)
-
-plt.figure(figsize=(7,6))
-plt.xlabel("Number of Neurons")
-plt.ylabel('Roc AUC')
-plt.suptitle("Roc curve integral for several configurations of Neural Nets", fontsize=13, fontweight='bold')
-plt.title("Learning rate: {0}\nDecay: {1}".format(learning_rate, my_decay), fontsize=10)
-# ===> TEM QUE SE AJUSTAR BEM O INICIO E FIM DAS LISTAS PARA CRIAR UMA LINAH POR LAYER/OUTRO PARAMETRO
-lim = len(neurons)/nLayers
-
-for i in range(0,nLayers):
-    plt.plot(neurons[i*lim:(i+1)*lim], roc_AUC[i*lim:(i+1)*lim])
+    parser = argparse.ArgumentParser(description='Process the command line options')
+#   parser.add_argument('-c', '--configFile', required=True, help='Configuration file describing the neural network topology and options as well as the samples to process')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Whether to print verbose output')
+    parser.add_argument('-f', '--file', required=True, help='File name')
+    parser.add_argument('-s', '--singleNN', action='store_true', help='Whether this NN is stored in the Searches or SingleNN folder')
+    parser.add_argument('-r', '--runNum', type=int, required=True, help='Run number')
+    parser.add_argument('-l', '--learningRate', type=float, required=True, help='Learning rate')
+    parser.add_argument('-d', '--decay', type=float, required=True, help='Learning rate decay')
 
 
-'''
-plt.plot(neurons[0:100], roc_AUC[0:100])
-plt.plot(neurons[100:200], roc_AUC[100:200])
-plt.plot(neurons[200:300], roc_AUC[200:300])
-plt.plot(neurons[300:400], roc_AUC[300:400])
-plt.plot(neurons[400:500], roc_AUC[400:500])
-plt.plot(neurons[500:600], roc_AUC[500:600])
-#plt.axvline(x=25, ymin=0, ymax = 1, linewidth=2, color='black')
-#plt.axvline(x=80, ymin=0, ymax = 1, linewidth=2, color='black')
-'''
-plt.legend(layers_legend, loc='best')
-plt.savefig("ROC_run"+str(runNum)+"_"+str(test_point)+"_"+str(learning_rate)+"_"+str(my_decay)+".png")
-plt.show()
+    args = parser.parse_args()
+
+    runNum = args.runNum
+    learning_rate = args.learningRate
+    my_decay = args.decay
+    test_point = "550_520"
+    filepath = cfg.lgbk+"Searches/run"+str(runNum)
+    os.chdir(filepath)
+
+    #f = open(filepath+name+'.txt', 'r')
+    # Until run5
+    #name = "mGS:outputs_run"+str(runNum)+"_"+test_point
+    name = "mGS:outputs_run"+str(runNum)+"_"+test_point+"_"+str(learning_rate)+"_"+str(my_decay)
+
+    f = open(name + '.txt', 'r')
+
+    layer = []
+    neurons = []
+    roc_AUC = []
+    ks = []
+    ks_s = []
+    ks_b = []
+    FOM = []
+    line_index=0
+
+    for line in f:
+        if line_index%7==0:
+            layer.append(float(line,))
+        if line_index%7==1:
+            neurons.append(float(line,))
+        if line_index%7==2:
+            roc_AUC.append(float(line,))
+        if line_index%7==3:
+            ks_s.append(float(line,))
+        if line_index%7==4:
+            ks_b.append(float(line,))
+        if line_index%7==5:
+            ks.append(float(line,))
+        if line_index%7==6:
+            FOM.append(float(line,))
+        line_index=line_index+1
+
+    layers_legend = ["1 layer","2 layers", "3 layers"]
+    nLayers = len(layers_legend)
+
+    plt.figure(figsize=(7,6))
+    plt.xlabel("Number of Neurons")
+    plt.ylabel('Roc AUC')
+    plt.suptitle("Roc curve integral for several configurations of Neural Nets", fontsize=13, fontweight='bold')
+    plt.title("Learning rate: {0}\nDecay: {1}".format(learning_rate, my_decay), fontsize=10)
+
+    lim = len(neurons)/nLayers
+
+    for i in range(0,nLayers):
+        plt.plot(neurons[i*lim:(i+1)*lim], roc_AUC[i*lim:(i+1)*lim])
+
+    plt.legend(layers_legend, loc='best')
+    plt.savefig("ROC_run"+str(runNum)+"_"+str(test_point)+"_"+str(learning_rate)+"_"+str(my_decay)+".png")
+    plt.show()
--- plotWeights.py
+++ plotWeights.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     if args.singleNN:
         filepath = cfg.lgbk + "SingleNN/" + model_name
     elif args.runNum != None:
-        filepath = cfg.lgbk + "Searches/run" + str(args.runNum)+ "/" + model_name
+        filepath = cfg.lgbk + "Searches/run" + str(args.runNum)
     elif args.local:
         filepath = "/home/diogo/PhD/SingleNN/" + model_name
 
