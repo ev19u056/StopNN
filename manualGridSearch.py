291081d016be08936de3fe63177f244cc7328b28
diogodebastos <diogodebastos18@gmail.com>
2018-02-06 16:53:14 +0000

    fix ident

--- manualGridSearch.py
+++ manualGridSearch.py
@@ -84,82 +84,82 @@ if __name__ == "__main__":
     	name = "L"+str(y)+"_N"+str(x)+"_"+train_DM+"_run"+str(runNum)
 
     	acc = history.history["acc"]
-            #val_acc = history.history['val_acc']
-            loss = history.history['loss']
-            #val_loss = history.history['val_loss']
-            pickle.dump(acc, open("accuracy/acc_"+name+".pickle", "wb"))
-            pickle.dump(loss, open("loss/loss_"+name+".pickle", "wb"))
-            model.save(name+".h5")
-            model_json = model.to_json()
-            with open(name + ".json", "w") as json_file:
-                json_file.write(model_json)
-                model.save_weights(name + ".h5")
+        #val_acc = history.history['val_acc']
+        loss = history.history['loss']
+        #val_loss = history.history['val_loss']
+        pickle.dump(acc, open("accuracy/acc_"+name+".pickle", "wb"))
+        pickle.dump(loss, open("loss/loss_"+name+".pickle", "wb"))
+        model.save(name+".h5")
+        model_json = model.to_json()
+        with open(name + ".json", "w") as json_file:
+            json_file.write(model_json)
+            model.save_weights(name + ".h5")
 
-            print("Getting predictions")
-            devPredict = model.predict(XDev)
-            valPredict = model.predict(XVal)
+        print("Getting predictions")
+        devPredict = model.predict(XDev)
+        valPredict = model.predict(XVal)
 
-            print("Getting scores")
+        print("Getting scores")
 
-            scoreDev = model.evaluate(XDev, YDev, sample_weight=weightDev, verbose = 0)
-            scoreVal = model.evaluate(XVal, YVal, sample_weight=weightVal, verbose = 0)
-            cohen_kappa=cohen_kappa_score(YVal, valPredict.round())
+        scoreDev = model.evaluate(XDev, YDev, sample_weight=weightDev, verbose = 0)
+        scoreVal = model.evaluate(XVal, YVal, sample_weight=weightVal, verbose = 0)
+        cohen_kappa=cohen_kappa_score(YVal, valPredict.round())
 
-            print "Calculating FOM:"
-            dataDev["NN"] = devPredict
-            dataVal["NN"] = valPredict
+        print "Calculating FOM:"
+        dataDev["NN"] = devPredict
+        dataVal["NN"] = valPredict
 
-            sig_dataDev=dataDev[dataDev.category==1]
-            bkg_dataDev=dataDev[dataDev.category==0]
-            sig_dataVal=dataVal[dataVal.category==1]
-            bkg_dataVal=dataVal[dataVal.category==0]
+        sig_dataDev=dataDev[dataDev.category==1]
+        bkg_dataDev=dataDev[dataDev.category==0]
+        sig_dataVal=dataVal[dataVal.category==1]
+        bkg_dataVal=dataVal[dataVal.category==0]
 
 
-            tmpSig, tmpBkg = getYields(dataVal)
-            sigYield, sigYieldUnc = tmpSig
-            bkgYield, bkgYieldUnc = tmpBkg
+        tmpSig, tmpBkg = getYields(dataVal)
+        sigYield, sigYieldUnc = tmpSig
+        bkgYield, bkgYieldUnc = tmpBkg
 
-            fomEvo = []
-            fomCut = []
+        fomEvo = []
+        fomCut = []
 
-            bkgEff = []
-            sigEff = []
+        bkgEff = []
+        sigEff = []
 
-            sig_Init = dataVal[dataVal.category == 1].weight.sum() * 35866 * 2
-            bkg_Init = dataVal[dataVal.category == 0].weight.sum() * 35866 * 2
+        sig_Init = dataVal[dataVal.category == 1].weight.sum() * 35866 * 2
+        bkg_Init = dataVal[dataVal.category == 0].weight.sum() * 35866 * 2
 
-            for cut in np.arange(0.0, 0.9999, 0.001):
-                sig, bkg = getYields(dataVal, cut=cut, luminosity=luminosity)
-                if sig[0] > 0 and bkg[0] > 0:
-                    fom, fomUnc = FullFOM(sig, bkg)
-                    fomEvo.append(fom)
-                    fomCut.append(cut)
-                    bkgEff.append(bkg[0]/bkg_Init)
-                    sigEff.append(sig[0]/sig_Init)
+        for cut in np.arange(0.0, 0.9999, 0.001):
+            sig, bkg = getYields(dataVal, cut=cut, luminosity=luminosity)
+            if sig[0] > 0 and bkg[0] > 0:
+                fom, fomUnc = FullFOM(sig, bkg)
+                fomEvo.append(fom)
+                fomCut.append(cut)
+                bkgEff.append(bkg[0]/bkg_Init)
+                sigEff.append(sig[0]/sig_Init)
 
-            max_FOM=0
+        max_FOM=0
 
-            print "Maximizing FOM"
-            for cv_0 in fomEvo:
-                if cv_0>max_FOM:
-                    max_FOM=cv_0
+        print "Maximizing FOM"
+        for cv_0 in fomEvo:
+            if cv_0>max_FOM:
+                max_FOM=cv_0
 
-            roc_Integral = 0
-            for cv_1 in range(0, len(bkgEff)-1):
-                roc_Integral=roc_Integral+0.5*(bkgEff[cv_1]-bkgEff[cv_1+1])*(sigEff[cv_1+1]+sigEff[cv_1])
+        roc_Integral = 0
+        for cv_1 in range(0, len(bkgEff)-1):
+            roc_Integral=roc_Integral+0.5*(bkgEff[cv_1]-bkgEff[cv_1+1])*(sigEff[cv_1+1]+sigEff[cv_1])
 
-            Eff = zip(bkgEff, sigEff)
+        Eff = zip(bkgEff, sigEff)
 
-            km_value_s = ks_2samp(sig_dataDev["NN"], sig_dataVal["NN"])[1]
-            km_value_b = ks_2samp(bkg_dataDev["NN"], bkg_dataVal["NN"])[1]
-            km_value = ks_2samp(dataDev["NN"], dataVal["NN"])[1]
+        km_value_s = ks_2samp(sig_dataDev["NN"], sig_dataVal["NN"])[1]
+        km_value_b = ks_2samp(bkg_dataDev["NN"], bkg_dataVal["NN"])[1]
+        km_value = ks_2samp(dataDev["NN"], dataVal["NN"])[1]
 
-            f.write(str(y)+"\n")
-            f.write(str(x)+"\n")
-            f.write(str(roc_Integral)+"\n")
-            f.write(str(km_value_s)+"\n")
-            f.write(str(km_value_b)+"\n")
-            f.write(str(km_value)+"\n")
-            f.write(str(max_FOM)+"\n")
+        f.write(str(y)+"\n")
+        f.write(str(x)+"\n")
+        f.write(str(roc_Integral)+"\n")
+        f.write(str(km_value_s)+"\n")
+        f.write(str(km_value_b)+"\n")
+        f.write(str(km_value)+"\n")
+        f.write(str(max_FOM)+"\n")
 
     sys.exit("Done!")
