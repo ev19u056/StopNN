bc484c42b4a9128aad9b853f016eec43fbdd7f87
diogodebastos <diogodebastos18@gmail.com>
2018-02-08 15:52:11 +0000

    Train with summer datasets

--- localConfig.py
+++ localConfig.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 #path to .root datasets
-loc = "/lstore/cms/cbeiraod/Stop4Body/"
 #loc = "/gstore/t3cms/store/user/cbeiraod/Stop4Body/storage/"
+#loc = "/lstore/cms/cbeiraod/Stop4Body/"
+loc = "/lstore/cms/dbastos/Stop4Body/"
+#loc = "/home/diogo/LIP/DATA/"
 #path to logbook
 lgbk = "/lstore/cms/dbastos/LogBook/"
